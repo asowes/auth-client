73379e65cdd342e9f6c4335eb8561f949116b83b
yangbo <1051435548@qq.com>
2022-08-15 23:33:32 +0800

    调整注册ui

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -40,48 +40,63 @@ function Login() {
     >
       <Particle />
 
-      <Flex color="#fff" alignItems="baseline" gap="64px">
-        <Box color="#ffffff" fontWeight="bold" fontSize="32px">
-          Walk On Air
-        </Box>
-        <PointerBox color="#9ca5b3" fontWeight="bold" fontSize="22px">
-          Home
-        </PointerBox>
-        <PointerBox color="#9ca5b3" fontWeight="bold" fontSize="22px">
-          Login
-        </PointerBox>
-      </Flex>
-      <Flex>
-        <Box mt="200px">
-          <Box>
-            <Box color="#9ca5b3" fontWeight="bold" fontSize="18px">
-              START FOR FREE
-            </Box>
-            <Box color="#ffffff" fontWeight="bold" fontSize="48px" mt="16px">
-              Create new account<span style={{ color: "#4490ee" }}>.</span>
-            </Box>
-            <Box color="#9ca5b3" fontSize="18px" mt="24px">
-              Already A Member？
-              <PointerBox as="span" style={{ color: "#4490ee" }}>
-                Log In
-              </PointerBox>
-            </Box>
-            <FormProvider {...method}>
-              <CreateAccountForm />
-            </FormProvider>
-            <Box mt="64px" />
-            <Button
-              onClick={(e) => {
-                handleSubmit(
-                  (data) => onFormSubmit(data, false),
-                  (errors) => onFormError(errors, false)
-                )(e);
-              }}
-            >
-              Create account
-            </Button>
+      <Flex color="#fff" justifyContent="space-between">
+        <Flex alignItems="flex-end" gap="64px">
+          <Box color="#ffffff" fontWeight="bold" fontSize="28px">
+            Walk On Air
           </Box>
-          <Box />
+          <PointerBox color="#9ca5b3" fontWeight="bold" fontSize="16px">
+            Home
+          </PointerBox>
+          <PointerBox color="#9ca5b3" fontWeight="bold" fontSize="16px">
+            Login
+          </PointerBox>
+        </Flex>
+        <Flex alignItems="flex-end" gap="64px">
+          <PointerBox color="#9ca5b3" fontWeight="bold" fontSize="16px">
+            Language
+          </PointerBox>
+          <PointerBox color="#9ca5b3" fontWeight="bold" fontSize="16px">
+            Contact US
+          </PointerBox>
+        </Flex>
+      </Flex>
+
+      <Flex mt="150px" justifyContent="flex-end" gap="48px">
+        <Box flex="1">
+          <img
+            // src="https://w.wallhaven.cc/full/j3/wallhaven-j3dg1m.jpg"
+            src="https://w.wallhaven.cc/full/dp/wallhaven-dpl3x3.jpg"
+            alt=""
+          />
+        </Box>
+        <Box>
+          <Box color="#9ca5b3" fontWeight="bold" fontSize="18px">
+            START FOR FREE
+          </Box>
+          <Box color="#ffffff" fontWeight="bold" fontSize="40px" mt="16px">
+            Create new account<span style={{ color: "#4490ee" }}>.</span>
+          </Box>
+          <Box color="#9ca5b3" fontSize="18px" mt="24px">
+            Already A Member？
+            <PointerBox as="span" style={{ color: "#4490ee" }}>
+              Log In
+            </PointerBox>
+          </Box>
+          <FormProvider {...method}>
+            <CreateAccountForm />
+          </FormProvider>
+          <Box mt="64px" />
+          <Button
+            onClick={(e) => {
+              handleSubmit(
+                (data) => onFormSubmit(data, false),
+                (errors) => onFormError(errors, false)
+              )(e);
+            }}
+          >
+            Create account
+          </Button>
         </Box>
       </Flex>
     </Box>
