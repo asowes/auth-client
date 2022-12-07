486b685230eca3d209a33fac9b62d9a9a9b74ef5
Yang Bo <yang7@michaels.coom>
2022-12-07 09:41:36 +0800

    日期计算器

---
+++ src/pages/compute-date/ComputeDate.style.js
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  padding: 64px;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const Title = styled.div`
+  font-size: 32px;
+  font-weight: 600;
+  margin-bottom: 64px;
+`;
+
+export const SubTitle = styled.div`
+  font-size: 24px;
+  font-weight: 600;
+`;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "../pages/login";
 import Register from "../pages/register";
+import EatWhat from "../pages/eatwhat";
+import ComputeDate from "../pages/compute-date";
 
 function AppRouter() {
   return (
@@ -11,6 +13,8 @@ function AppRouter() {
         <Route path="/" exact component={Login} />
         <Route path="/register" exact component={Register} />
         <Route path="/sign-in" exact component={Login} />
+        <Route path="/eat-what" exact component={EatWhat} />
+        <Route path="/compute-date" exact component={ComputeDate} />
       </Switch>
     </Router>
   );
